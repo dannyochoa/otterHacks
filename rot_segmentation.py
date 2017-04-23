02d1eacba5b4ec7e040744052120fc2deeb16084
sampsonliao <sliao@csumb.edu>
2017-04-23 01:27:27 -0700

    write rot contour to file

--- rot_segmentation.py
+++ rot_segmentation.py
@@ -1,15 +1,12 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
-
 
 def estimate_area(contour_array):
     """Returns a list of areas enclosed by contours
     """
     areas = []
     for cnt in contour_array:
-        m = cv2.moments(cnt)
         area = cv2.contourArea(cnt)
         areas.append(area)
     return areas
@@ -21,9 +18,9 @@ def find_contours(low, up, frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, low, up)
     res = cv2.bitwise_and(frame, frame, mask=mask)
-    cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask)
-    cv2.imshow('res', res)
+    # cv2.imshow('frame', frame)
+    # cv2.imshow('mask', mask)
+    # cv2.imshow('res', res)
     cv2.waitKey(0)
     blurred = cv2.pyrMeanShiftFiltering(res, 30, 30)
     gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
@@ -38,26 +35,32 @@ if __name__ == "__main__":
 
     low_leaf = np.array([30, 100, 70])
     up_leaf = np.array([70, 255, 255])
+    #
+    grape_leaf = cv2.imread("Newly-Infected-Leaf-Figure-1.png")
 
-    grape_leaf = cv2.imread("1 copy.jpg")
-
-    gl_rot_edit = gl_leaf_edit = grape_leaf
+    gl_rot_edit = grape_leaf.copy()
+    gl_leaf_edit = grape_leaf.copy()
 
     rot_contours = find_contours(low_rot, up_rot, grape_leaf)
     cv2.drawContours(gl_rot_edit, rot_contours, -1, (0, 0, 255), 2)
+    cv2.imwrite('rot.png', gl_rot_edit)
 
     leaf_contours = find_contours(low_leaf, up_leaf, grape_leaf)
-    cv2.drawContours(gl_leaf_edit, leaf_contours, -1, (0, 0, 255), 2)
+    # cv2.drawContours(gl_leaf_edit, leaf_contours, -1, (0, 0, 255), 2)
+    # cv2.imwrite('leaf.png', gl_leaf_edit)
 
-    cv2.imshow('orange', gl_rot_edit)
-    cv2.waitKey(0)
-    cv2.imshow('orange', gl_leaf_edit)
-    cv2.waitKey(0)
-
+    # cv2.imshow('orange', gl_rot_edit)
+    # cv2.waitKey(0)
+    # cv2.imshow('orange', gl_leaf_edit)
+    # cv2.waitKey(0)
+    #
     rot_areas = estimate_area(rot_contours)
     leaf_areas = estimate_area(leaf_contours)
 
     total_rot_area = sum(rot_areas)
     total_leaf_area = sum(leaf_areas)
 
-    print("Leaf is ", (total_rot_area / total_leaf_area) * 100.00, "% rot")
+    rot_ratio = float(total_rot_area/total_leaf_area)*100.00
+    print(rot_ratio)
+    # print("Leaf is ", (total_rot_area / total_leaf_area) * 100.00, "% rot")
+
